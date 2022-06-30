a21e811c5ea2b7f735f2703c8fa3213b351ca8e1
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-06-30 19:19:37 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,6 @@
+/*Task
+The provided code stub reads and integer, , from STDIN. For all non-negative integers i<n , print i^2 .*/
+n = int(input())
+for i in range(0,n):
+    if(i<n):
+            print(i**2)
