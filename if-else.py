66feac6c1694d79ffdea20bc407c3ef36739a1ff
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-06-30 19:10:59 +0530

    if-else

---
+++ if-else.py
@@ -0,0 +1,17 @@
+/*Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird */
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")  
